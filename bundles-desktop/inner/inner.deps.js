06e572acb64d1c3f9c368fcdc073a735655ba237
tavria <tavria@yandex-team.ru>
2013-04-26 15:20:42 +0300

    add new block article-author

---
+++ bundles-desktop/inner/inner.deps.js
@@ -0,0 +1,272 @@
+exports.deps = [
+    {
+        "block": "i-jquery"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "inherit"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "identify"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "is-empty-object"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "debounce"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "observable"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "object"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "array"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "function"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "i-bem",
+        "elem": "i18n",
+        "mod": "dummy",
+        "val": "yes"
+    },
+    {
+        "block": "i-bem",
+        "elem": "html"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "json"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "string"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "i-ua"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "core"
+    },
+    {
+        "block": "b-page"
+    },
+    {
+        "block": "b-page",
+        "elem": "css"
+    },
+    {
+        "block": "b-page",
+        "elem": "js"
+    },
+    {
+        "block": "i-bem",
+        "elem": "i18n"
+    },
+    {
+        "block": "i-font"
+    },
+    {
+        "block": "i-font",
+        "mod": "face"
+    },
+    {
+        "block": "i-font",
+        "mod": "face",
+        "val": "alsekibastuz"
+    },
+    {
+        "block": "i-clearfix"
+    },
+    {
+        "block": "highlight"
+    },
+    {
+        "block": "highlight",
+        "elem": "code"
+    },
+    {
+        "block": "highlight",
+        "mod": "theme"
+    },
+    {
+        "block": "highlight",
+        "mod": "theme",
+        "val": "github"
+    },
+    {
+        "block": "gh-ribbon"
+    },
+    {
+        "block": "header"
+    },
+    {
+        "block": "i-menu"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "leftclick"
+    },
+    {
+        "block": "b-menu-horiz"
+    },
+    {
+        "block": "b-menu-horiz",
+        "mod": "layout"
+    },
+    {
+        "block": "b-menu-horiz",
+        "elem": "title"
+    },
+    {
+        "block": "b-menu-horiz",
+        "mod": "layout",
+        "val": "normal"
+    },
+    {
+        "block": "b-menu-horiz",
+        "mod": "type"
+    },
+    {
+        "block": "b-menu-horiz",
+        "mod": "type",
+        "val": "navigate"
+    },
+    {
+        "block": "b-menu-horiz",
+        "mod": "type",
+        "val": "submenu"
+    },
+    {
+        "block": "header",
+        "elem": "logo"
+    },
+    {
+        "block": "b-link"
+    },
+    {
+        "block": "b-menu-horiz",
+        "elem": "item"
+    },
+    {
+        "block": "b-menu-horiz",
+        "elem": "item",
+        "mod": "state"
+    },
+    {
+        "block": "b-menu-horiz",
+        "elem": "item",
+        "mod": "state",
+        "val": "parent"
+    },
+    {
+        "block": "b-menu-horiz",
+        "elem": "item",
+        "mod": "state",
+        "val": "current"
+    },
+    {
+        "block": "lang-switcher"
+    },
+    {
+        "block": "nav"
+    },
+    {
+        "block": "nav",
+        "mod": "type"
+    },
+    {
+        "block": "nav",
+        "mod": "type",
+        "val": "main"
+    },
+    {
+        "block": "nav",
+        "mod": "type",
+        "val": "sub"
+    },
+    {
+        "block": "content"
+    },
+    {
+        "block": "b-text"
+    },
+    {
+        "block": "b-text",
+        "elem": "h1"
+    },
+    {
+        "block": "b-text",
+        "elem": "p"
+    },
+    {
+        "block": "b-text",
+        "elem": "h2"
+    },
+    {
+        "block": "b-text",
+        "elem": "ul"
+    },
+    {
+        "block": "b-text",
+        "elem": "li"
+    },
+    {
+        "block": "b-text",
+        "elem": "h3"
+    },
+    {
+        "block": "b-text",
+        "elem": "h4"
+    },
+    {
+        "block": "footer"
+    },
+    {
+        "block": "footer",
+        "elem": "copyright"
+    },
+    {
+        "block": "metrika"
+    },
+    {
+        "block": "article-author"
+    }
+
+];
+exports.depsByTechs = {
+    "": {}
+};
